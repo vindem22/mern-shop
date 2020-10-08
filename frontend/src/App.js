017e20c9467af4b20b8d19ea897afdd2f8fd16b7
Zhanbolat Bekmaganbetov <bekmaganbetov.janbolat@gmail.com>
2020-10-08 21:04:49 +0600

    Basic styles for header,footer and main section. Added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <h1>Welcome to MERN shop</h1>
+      <Header></Header>
+      <main className="py-4">
+        <Container>
+          <h1>Welcome to VinShop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
     </>
   );
 }
