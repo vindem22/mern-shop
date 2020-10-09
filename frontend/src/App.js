3d8b4348a24913affc5f6b5015e1076180863144
Zhanbolat Bekmaganbetov <bekmaganbetov.janbolat@gmail.com>
2020-10-09 23:07:50 +0600

    Added Product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -12,8 +12,8 @@ function App() {
       <Header></Header>
       <main className="py-4">
         <Container>
-          <Route path='/' component={HomeScreen} exact></Route>
-          <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
         </Container>
       </main>
       <Footer></Footer>
