0863e13196bf27ef6f9bdcece4a17e0ba54c75c3
Zhanbolat Bekmaganbetov <bekmaganbetov.janbolat@gmail.com>
2020-10-18 02:15:25 +0600

    Adding routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,36 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+const router = express.Router();
+
+// @desc Fetch all products
+// @route GET api/products
+// @access Public route
+router.get(
+  "/",
+  // express-async-handler
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// @desc Fetch single product
+// @route GET api/:id
+// @access Public route
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product is not found" });
+    }
+
+    res.json(product);
+  })
+);
+
+export default router;
